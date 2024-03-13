cd09dfe5ebb0ebcc4312f84c718d8be9a0b1cfb5
Pavel <podoprigoraps@gmail.com>
2024-03-13 19:11:04 +0400

    Сделана функция получения операционной системы и корневой папки

--- static/script.js
+++ static/script.js
@@ -17,40 +17,45 @@ let currentPath = ''
 document.addEventListener('DOMContentLoaded', async function () {
     // установка операционной системы и корневой директории пользователя
     const userDataURL = `/user-info`
-    setDefaultPathAndOS(userDataURL)
+    await getSetDefaultPathAndOS(userDataURL)
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
     currentPath = defaultPath
 
     let pathInput = document.getElementById('path')
     pathInput.value = defaultPath
     const url = `/paths?sortField=${encodeURIComponent(defaultSortField)}&sortOrder=${encodeURIComponent(defaultSortOrder)}&path=${encodeURIComponent(currentPath)}`
 
-    getPaths(url)
+    await getPaths(url)
 });
 
 // setDefaultPathAndOS получает данные об операционной системе и корневой директории пользователя
 // и устанавливает соответствующие значения глобальных переменных
-function setDefaultPathAndOS(url) {
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => {
+async function getSetDefaultPathAndOS(url) {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
         if (!response.ok) {
             throw new Error('Ошибка. Данные не получены (статус отличен от 200 OK). Причина:')
         }
-        return response.json()
-    })
-    .then(data => {
-        // если статус с сервера не удовлетворительный, то вывод ошибки
+
+        const data = await response.json();
+
         if (data.serverIsSucceed == false) {
             throw new Error('Ошибка. Данные получены (статус 200 OK), но они пусты. Причина: ' + data.serverErrorText)
         }
+
+        // вывод времени 
+        document.querySelector('.time').innerText = `Загружено за: ${data.loadTime} секунд`
+
         // временно:
         // defaultPath = data.serverData.rootDir
+
+        // определение операционной системы и корневой папки
         os = data.serverData.os
         if (os == WINDOWS) {
             defaultPath = 'F:'
@@ -58,102 +63,99 @@ function setDefaultPathAndOS(url) {
         else if (os == LINUX) {
             defaultPath = '/'
         }
-        
-    })
-    .catch(error => {
+    } catch(error) {
         console.error('Ошибка запроса:', error)
-    })
-};
+        throw error
+    }
+}
 
 // getPaths получает данные с сервера и создает элементы на странице
-function getPaths(url) {
-    fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => {
-        let errorDiv = document.querySelector('.error-data-not-found')
-        if (!response.ok) {
-            // удаление всех ранее отрисованных путей (потомков списка)
-            let itemList = document.querySelector('.path-list-section')
-            while (itemList.firstChild) {
-                itemList.removeChild(itemList.firstChild)
+async function getPaths(url) {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
             }
-            // вывод ошибки
+        })
+
+        // удаление предыдущих отрисованных путей
+        clearPathList()
+        
+        let errorDiv = document.querySelector('.error-data-not-found')
+        
+        if (!response.ok) {
             errorDiv.classList.add('error-data-not-found-active')
             throw new Error('Ошибка. Данные не получены (статус отличен от 200 OK). Причина:')
         }
+        
         // сокрытие ошибки
         errorDiv.classList.remove('error-data-not-found-active')
-        return response.json()
-    })
-    .then(data => {
-        let errorDiv = document.querySelector('.error-data-not-found')
-        // если статус с сервера не удовлетворительный, то вывод ошибки
+        
+        const data = await response.json()
+
+        // если внутренний статус с сервера не удовлетворительный, то отображение ошибки
         if (data.serverIsSucceed == false) {
-            // удаление всех ранее отрисованных путей (потомков списка)
-            let itemList = document.querySelector('.path-list-section')
-            while (itemList.firstChild) {
-                itemList.removeChild(itemList.firstChild)
-            }
-            // вывод ошибки
             errorDiv.classList.add('error-data-not-found-active')
             throw new Error('Ошибка. Данные получены (статус 200 OK), но они пусты. Причина: ' + data.serverErrorText)
         }
+        
         // вывод времени
         document.querySelector('.time').innerText = `Загружено за: ${data.loadTime} секунд`
 
-        // вызов функции отрисовки путей
+        // вызов функции отрисовки новых путей
         createNewElements(data.serverData)
-    })
-    .catch(error => {
+    } catch (error) {
         console.error('Ошибка запроса:', error)
-    })
+        throw error
+    }
 };
 
+// clearPathList удаляет все пути из списка
+function clearPathList() {
+    let itemList = document.querySelector('.path-list-section');
+    while (itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild);
+    }
+}
+
 // createNewElements отрисовывает полученные с сервера пути в качестве потомков списка
-function createNewElements(data) {
-    let pathList = document.querySelector('.path-list-section')
+function createNewElements(paths) {
+    clearPathList()
 
-        while (pathList.firstChild) {
-            pathList.removeChild(pathList.firstChild)
-        }
+    for (let i = 0; i < paths.length; i++) {
+        // создание элемента нового пути
+        let newPath = document.createElement('div')
+        newPath.classList.add('path-list__item')
 
-        for (let i = 0; i < data.length; i++) {
-            // создаем элемент нового пути
-            let newPath = document.createElement('div')
-            newPath.classList.add('path-list__item')
+        // если путь является папкой, добавление соответствующего класса и кликабельности для него
+        if (paths[i].type == "Папка") {
+            newPath.classList.add('path-list__item_folder')
 
-            // если путь является папкой, добавляем соответствующий класс и возможность нажать на него
-            if (data[i].type == "Папка") {
-                newPath.classList.add('path-list__item_folder')
-
-                newPath.onclick = function () {
-                    setNewPath(data[i].relPath)
-                }
+            newPath.onclick = function () {
+                setNewPath(paths[i].relPath)
             }
-
-            // инициализируем составляющие информации о путе: название, тип, размер и дату редактирования
-            const pathComponents = [
-                { text: data[i].relPath, class: 'path-list__item__component' },
-                { text: data[i].type, class: 'path-list__item__component' },
-                { text: data[i].itemSize, class: 'path-list__item__component' },
-                { text: data[i].editDate, class: 'path-list__item__component' }
-            ]
-
-            // для каждого такого составляющего отрисовываем его в качестве потомка пути, которому они принадлежат
-            pathComponents.forEach(element => {
-                let newComponent = document.createElement('div')
-                newComponent.textContent = element.text
-                newComponent.classList.add(element.class)
-                newPath.appendChild(newComponent)
-            })
-
-            // добавляем созданный элемент пути в список
-            pathList.appendChild(newPath)
         }
+
+        // инициализация составляющих информации о пути: название, тип, размер и дату редактирования
+        const pathComponents = [
+            { text: paths[i].relPath, class: 'path-list__item__component' },
+            { text: paths[i].type, class: 'path-list__item__component' },
+            { text: paths[i].itemSize, class: 'path-list__item__component' },
+            { text: paths[i].editDate, class: 'path-list__item__component' }
+        ]
+
+        // отрисовка каждого такого составляющего в качестве потомка соответствующего ему пути
+        pathComponents.forEach(element => {
+            let newComponent = document.createElement('div')
+            newComponent.textContent = element.text
+            newComponent.classList.add(element.class)
+            newPath.appendChild(newComponent)
+        })
+
+        // добавление созданного элемента пути в список
+        pathList.appendChild(newPath)
+    }
 }
 
 // sortTable обрабатывает нажатие кнопки сортировки (определяет порядок)
@@ -174,11 +176,15 @@ function setDefaultPath() {
 
 // setPreviousPath устанавливает значение текущего пути на 1 уровень выше
 function setPreviousPath() {
+    // получение массива из текущего пути
     let pathInput = document.getElementById('path')
     let pathArray = currentPath.split('/')
+
+    // если в Windows путь вида "ДИСК:", то возврат
     if (os === WINDOWS && pathArray.length < 2) {
         return
     } 
+    // если в Linux путь вида "/home", то возврат
     else if (os === LINUX && pathArray.length <= 2) {
         currentPath = "/"
         const url = `/paths?sortField=${encodeURIComponent(defaultSortField)}&sortOrder=${encodeURIComponent(defaultSortOrder)}&path=${encodeURIComponent(currentPath)}`
@@ -186,13 +192,14 @@ function setPreviousPath() {
         pathInput.value = currentPath
         return
     }
-    else {
-        pathArray.pop()
-        currentPath = pathArray.join('/')
-        const url = `/paths?sortField=${encodeURIComponent(defaultSortField)}&sortOrder=${encodeURIComponent(defaultSortOrder)}&path=${encodeURIComponent(currentPath)}`
-        getPaths(url)
-        pathInput.value = currentPath
-    }
+
+    // удаление последнего составляющее пути и соединение его обратно в строку
+    pathArray.pop()
+    currentPath = pathArray.join('/')
+
+    const url = `/paths?sortField=${encodeURIComponent(defaultSortField)}&sortOrder=${encodeURIComponent(defaultSortOrder)}&path=${encodeURIComponent(currentPath)}`
+    getPaths(url)
+    pathInput.value = currentPath
 };
 
 // setPreviousPath устанавливает значение текущего пути в зависимости от директории, в которую пользователь перешел
