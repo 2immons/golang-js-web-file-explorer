da10e256251c5f16ee1f6eed8de1367212d05d6c
Pavel <71785021+2immons@users.noreply.github.com>
2024-03-20 19:31:55 +0400

    Delete src/JSscripts directory

--- src/JSscripts/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-import '../style.css';
-import Controller from './controller.js';
-import View from './view.js';
-const controller = new Controller();
-const view = new View(controller);
-controller.init();
