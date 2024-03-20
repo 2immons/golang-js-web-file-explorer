ebf98a83bcc3399efb1e1e6fe942cff0e0de605b
Pavel <podoprigoraps@gmail.com>
2024-03-20 09:58:12 +0400

    try catch php

--- apache-scripts/php-scripts/statGet.php
+++ apache-scripts/php-scripts/statGet.php
@@ -1,63 +1,69 @@
 <?php
-// проверка, что пришел GET запрос
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    // подключение к базе данных
-    $servername = "localhost";
-    $username = "root";
-    $password = "Orig195025";
-    $dbname = "statDB";
+try {
+    // проверка, что пришел GET запрос
+    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        // подключение к базе данных
+        $servername = "localhost";
+        $username = "root";
+        $password = "Orig195025";
+        $dbname = "statDB";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // проверка соединения
-    if ($conn->connect_error) {
-        die("Соединение не удалось: " . $conn->connect_error);
-    }
-
-    // SQL запрос для выборки данных
-    $sql = "SELECT id, total_size, date_time, root_path, load_time_seconds FROM stat";
-    $query_result = $conn->query($sql);
-
-    // формирование HTML таблицы на основе данных из базы данных
-    $table = "
-        <a href='http://localhost:8324'>Вернуться</a>
-        <table border='2'>
-        <tr>
-            <th>№</th>
-            <th>Размер</th>
-            <th>Дата,время</th>
-            <th>Путь</th>
-            <th>Время</th>
-        </tr>";
-    if ($query_result->num_rows > 0) {
-        while($row = $query_result->fetch_assoc()) {
-            $table .= "<tr>
-                <td>".$row["id"]."</td>
-                <td>".$row["total_size"]."</td>
-                <td>".$row["date_time"]."</td>
-                <td>".$row["root_path"]."</td>
-                <td>".$row["load_time_seconds"]."</td>
-            </tr>";
+        // проверка соединения
+        if ($conn->connect_error) {
+            throw new Exception("Соединение не удалось: " . $conn->connect_error);
         }
+
+        // SQL запрос для выборки данных
+        $sql = "SELECT id, total_size, date_time, root_path, load_time_seconds FROM stat";
+        $query_result = $conn->query($sql);
+
+        // формирование HTML таблицы на основе данных из базы данных
+        $table = "
+            <a href='http://localhost:8324'>Вернуться</a>
+            <table border='2'>
+            <tr>
+                <th>№</th>
+                <th>Размер</th>
+                <th>Дата,время</th>
+                <th>Путь</th>
+                <th>Время</th>
+            </tr>";
+        if ($query_result->num_rows > 0) {
+            while($row = $query_result->fetch_assoc()) {
+                $table .= "<tr>
+                    <td>".$row["id"]."</td>
+                    <td>".$row["total_size"]."</td>
+                    <td>".$row["date_time"]."</td>
+                    <td>".$row["root_path"]."</td>
+                    <td>".$row["load_time_seconds"]."</td>
+                </tr>";
+            }
+        } else {
+            $table .= "<tr><td colspan='5'>Нет данных о запросах</td></tr>";
+        }
+        $table .= "</table>";
+
+        // закрытие соединения с базой данных
+        $conn->close();
+
+        // установка заголовка HTTP для указания типа содержимого как HTML
+        header("Content-Type: text/html");
+
+        // вывод таблицы HTML
+        http_response_code(200);
+        header("Access-Control-Allow-Origin: *");
+        echo $table;
     } else {
-        $table .= "<tr><td colspan='5'>Нет данных о запросах</td></tr>";
+        // если запрос не GET, отправка сообщения об ошибке
+        http_response_code(405);
+        header("Content-Type:  application/json");
+        echo json_encode(array('message' => "Ошибка запроса PHP"));
     }
-    $table .= "</table>";
-
-    // закрытие соединения с базой данных
-    $conn->close();
-
-    // установка заголовка HTTP для указания типа содержимого как HTML
-    header("Content-Type: text/html");
-
-    // вывод таблицы HTML
-    http_response_code(200);
-    header("Access-Control-Allow-Origin: *");
-    echo $table;
-} else {
-    // если запрос не GET, отправка сообщения об ошибке
-    http_response_code(405);
+} catch (Exception $e) {
+    http_response_code(500);
     header("Content-Type:  application/json");
-    echo json_encode(array('message' => "Ошибка запроса PHP"));
+    echo json_encode(array('message' => $e->getMessage()));
 }
 ?>
--- apache-scripts/php-scripts/statPost.php
+++ apache-scripts/php-scripts/statPost.php
@@ -1,36 +1,42 @@
 <?php
-// проверка, что пришел POST запрос
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+try {
+    // проверка, что пришел POST запрос
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // подключение к базе данных
-    $servername = "localhost";
-    $username = "root";
-    $password = "Orig195025";
-    $dbname = "statDB";
+        // подключение к базе данных
+        $servername = "localhost";
+        $username = "root";
+        $password = "Orig195025";
+        $dbname = "statDB";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // проверка соединения
-    if ($conn->connect_error) {
-        die("Соединение не удалось: " . $conn->connect_error);
+        // проверка соединения
+        if ($conn->connect_error) {
+            die("Соединение не удалось: " . $conn->connect_error);
+        }
+        // получение данных из тела запроса
+        $data = json_decode(file_get_contents('php://input'), true);
+
+        // подготовка SQL запроса для вставки в таблицу
+        $sql = "INSERT INTO stat (total_size, date_time, root_path, load_time_seconds) VALUES (?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("isss", $data['totalSize'], $data['date'], $data['path'], $data['loadTime']);
+        
+        // выполнение SQL запроса
+        $stmt->execute();
+        $stmt->close();
+        
+        // закрытие соединения с базой данных
+        $conn->close();
+    } else {
+        // если запрос не POST, отправка сообщения об ошибке
+        http_response_code(405);
+        echo "Некорректный запрос (неверный тип, ожидался POST)";
     }
-    // получение данных из тела запроса
-    $data = json_decode(file_get_contents('php://input'), true);
-
-    // подготовка SQL запроса для вставки в таблицу
-    $sql = "INSERT INTO stat (total_size, date_time, root_path, load_time_seconds) VALUES (?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("isss", $data['totalSize'], $data['date'], $data['path'], $data['loadTime']);
-    
-    // выполнение SQL запроса
-    $stmt->execute();
-    $stmt->close();
-    
-    // закрытие соединения с базой данных
-    $conn->close();
-} else {
-    // если запрос не POST, отправка сообщения об ошибке
-    http_response_code(405);
-    echo "Некорректный запрос (неверный тип, ожидался POST)";
+} catch (Exception $e) {
+    http_response_code(500);
+    header("Content-Type:  application/json");
+    echo json_encode(array('message' => $e->getMessage()));
 }
 ?>
